77acd38d1fcbfc683291b5c583fc465675401b3f
shivani-5548 <90037384+shivani-5548@users.noreply.github.com>
2024-03-31 10:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+function swapTheme() {
+    var app = document.getElementById('app');
+    var swapButton = document.getElementById('swap');
+
+    if (app.classList.contains('day')) {
+        app.classList.remove('day');
+        app.classList.add('night');
+        swapButton.classList.remove('button_day');
+        swapButton.classList.add('button_night');
+    } else {
+        app.classList.remove('night');
+        app.classList.add('day');
+        swapButton.classList.remove('button_night');
+        swapButton.classList.add('button_day');
+    }
+}
